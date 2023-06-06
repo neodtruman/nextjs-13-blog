c32d0f796be2b063c3a9381d3d1104e1a6edc6dc
Neo D. Truman <neodtruman@gmail.com>
2023-06-06 12:25:50 +0700

    Chapter 5.1.2

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,3 +1,33 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 export default function AllPostsPage() {
-  return <h1>All Posts</h1>;
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('/posts.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data.posts);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h1>All Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
